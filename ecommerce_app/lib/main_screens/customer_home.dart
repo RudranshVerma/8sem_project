e83d1f01fb7bd3efb14e55819b823b52330f4630
RudranshVerma <83331599+RudranshVerma@users.noreply.github.com>
2022-11-19 11:51:24 +0530

    first commit

---
+++ ecommerce_app/lib/main_screens/customer_home.dart
@@ -0,0 +1,65 @@
+import 'package:ecommerce_app/main_screens/category.dart';
+import 'package:ecommerce_app/main_screens/home.dart';
+import 'package:ecommerce_app/main_screens/profile.dart';
+import 'package:ecommerce_app/main_screens/store.dart';
+import 'package:flutter/material.dart';
+
+import 'cart.dart';
+
+class CustomerHomeScreen extends StatefulWidget {
+  const CustomerHomeScreen({super.key});
+
+  @override
+  State<CustomerHomeScreen> createState() => _CustomerHomeScreenState();
+}
+
+class _CustomerHomeScreenState extends State<CustomerHomeScreen> {
+  int _selectedIndex = 0;
+  final List<Widget> _tabs = [
+    HomeScreen(),
+    CategoryScreen(),
+    StoresScreen(),
+    CartScreen(),
+    ProfileScreen(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _tabs[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        elevation: 0,
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Colors.black,
+        currentIndex: _selectedIndex,
+        selectedLabelStyle: const TextStyle(fontWeight: FontWeight.w600),
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Category',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shop),
+            label: 'Stores',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: 'Cart',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index; // telling that i have tapped on this icon
+          });
+        },
+      ),
+    );
+  }
+}
